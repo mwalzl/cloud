139756bc8c0eccc763ce560cb47d4ae109d25b6e
Martin Walzl <martin.walzl@edu.fh-joanneum.at>
2021-01-10 18:06:18 +0100

    Update Serverside.js

--- serverside.js
+++ serverside.js
@@ -70,6 +70,42 @@ const options = {
 
 const req = https.request(options, res => {
     console.log(`statusCode: ${res.statusCode}`)
+
+    var express = require("express");
+    var app = express();
+    app.use(express.logger());
+
+    app.get('/', function(request, response) {
+        response.send('Hello World!');
+        console.log("hello roland");
+    });
+
+    var port = process.env.PORT || 5000;
+    app.listen(port, function() {
+        console.log("Listening on " + port);
+    });
+
+    var pg = require('pg');
+
+    var connectionString = "postgres://snippet:keines@postgresql.myproject.svc.cluster.local:5432/snippet"
+
+   /* pg.connect(connectionString, function(err, client, done) {
+        client.query('INSERT INTO snippets VALUES(
+        12346789,
+            'Hello World',
+            'print a String Value',
+            'john',
+            'Java',
+            'System.println("Hello World");',
+            '{"simple", "beginner","print"}'
+    );'
+    , function(err, result) {
+            done();
+            if(err) return console.error(err);
+            console.log(result.rows);
+        });
+    })*/
+
     res.on('data', d => {
         process.stdout.write(d)
     })
